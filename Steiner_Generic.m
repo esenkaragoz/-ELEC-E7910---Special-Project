3933edaf2232d257770dd1d7f7fe488a197f97d9
esenkaragoz <99672463+esenkaragoz@users.noreply.github.com>
2022-02-14 18:06:41 +0200

    Update Steiner_Generic.m

--- Steiner_Generic.m
+++ Steiner_Generic.m
@@ -1,10 +1,14 @@
+% This code checks if the given set of elements fits to Steiner system definition. For each given .txt file, only these three parameters need to
+% be changed according to the Steiner system variables :  tForSteinerSet, kForSteinerSet, totNumOfSubsetInResult. Code will return immediately when
+% it finds overlaps between 2 subsets, otherwise will print "This is a Steiner System" text at the end.
+
 %Steiner = importdata('Steiner_2_3_7.txt');   %7
 %Steiner = importdata('Steiner_2_3_6.txt');   %6
 %Steiner = importdata('Steiner_3_5_26.txt');  %260
 Steiner = importdata('Steiner_3_5_65.txt'); %4368
 
-tForSteinerSet = 3; % number of elements won't appear together in any block more than once(t) 
-kForSteinerSet = 5; % number of element that subset will include
+tForSteinerSet = 3; % number of elements that won't appear together in any block more than once(t) 
+kForSteinerSet = 5; % number of elements that each subset will include (k)
 totNumOfSubsetInResult = 4368;
 for c=1:1:totNumOfSubsetInResult %number of result in C --> how many subsets
     for t=c+1:1:totNumOfSubsetInResult %c+1 bec we need to compare it with the next subset until the end 
