be80324cccd14930800ec6772d5f88463b71cb47
esenkaragoz <99672463+esenkaragoz@users.noreply.github.com>
2022-03-21 12:57:51 +0200

    Update DecoderErrorDetection.m
    Final version of simulation code for 1 frame

--- DecoderErrorDetection.m
+++ DecoderErrorDetection.m
@@ -2,58 +2,83 @@
 Steiner = importdata('Steiner_3_5_26.txt');
 
 numOfSlots = 26;
-numOfMaxActUser = 3; % number of maximum active user should be exist in a slot.
 [m,n] = size(Steiner);
-userRntiwithScheduledSlotNumbers = zeros(m,n+1); %initialize- n+1 because I want to reserve 1 column for RNTI numbers and 
-                                                 %next 5 columns for scheduled slot numbers
-                                               
-%to assign each Steiner System element to different users as a RNTI
-userRntiwithScheduledSlotNumbers(:,1) = linspace(1, m, m); %assigning increasing number from 1 to 260
+%n+1 because I want to reserve 1 column for RNTI numbers and
+%next 5 columns for scheduled slot numbers for those users
+userRntiwithScheduledSlotNumbers = zeros(m,n+1); %initialize
+                                                                                                       
+%to assign RNTI number from 1 to 260
+userRntiwithScheduledSlotNumbers(:,1) = linspace(1, m, m);
 %to assign scheduled slot numbers from Steiner System to users
 userRntiwithScheduledSlotNumbers(1:m,2:n+1) = Steiner(1:m,1:n);
 
-%there can be so many active users in the system(can not be known) but ideal one should be 3
-%according to the Steiner system(at least idea is that it should be possible to decode 3 active
-%user for 3_5_26). I created vector array for 5 users for now.In the vector array: 
-%1st column to keep how many active users in that slot
-%2nd, 3rd, 4th, 5th, and 6th columns to keep the RNTI numbers of these active users
-vector = zeros(numOfSlots,n+1); % Initializing all the slots with 0 active user at the beginning.
+%poisson distribution will be used to determine number of active users in the slots.
+%The range for lambda will be decided later. it is 3 for now.
+lambda = 3;
+numOfActiveUsers = poissrnd(lambda);
 
-%number of active users update in the scheduler
-%It will be replaced with poisson distribution next
-for i=1:1:numOfSlots
-    vector(i,1) = randi([1 n],1); %randomly generated number of active users in the slots.
-    vector(i,2:vector(i,1)+1) = randperm(m,vector(i,1)); %assign random & unique RNTI numbers for active users.
+%assign random RNTI numbers for active users between 1-260.
+activeUserArray = zeros(numOfActiveUsers,1);
+activeUserArray(:) = randperm(m,numOfActiveUsers);
+
+%slotVector to keep the scheduled RNTI numbers for the slots
+slotVector = zeros(numOfSlots,50); % Initializing all the slots with 0 active user at the beginning.
+
+%prepare scheduled slot vector from the userRntiwithScheduledSlotNumbers according to randomly chosen RNTI values
+slotUeCnt = ones(numOfSlots,1);
+for i=1:1:size(activeUserArray)
+  currentRnti = activeUserArray(i);
+  for j=2:1:n+1
+    slotNumOfUe = userRntiwithScheduledSlotNumbers(currentRnti,j);
+    slotVector(slotNumOfUe, slotUeCnt(slotNumOfUe)) = currentRnti;
+    slotUeCnt(slotNumOfUe) = slotUeCnt(slotNumOfUe)+1;
+  end
 end
 
-%decoder to check how many active users in the slot and update the vector after decode the users & apply SIC concept
-ue_set = zeros(260,1);
+%decoder to decode slots if there is no interference update the slotVector after decoding
 for i=1:1:numOfSlots
-    numOfActUsers = vector(i,1);
-    for j = 2:1:numOfActUsers+1
-        if ue_set(vector(i,j)) == 1
-            del_cnt = 1;
-            fprintf("deleted rnti %d from slot %d\n",vector(i,j), i);
-            for k = j+1:1:numOfActUsers+1
-                if ue_set(vector(i,k)) ~= 1
-                    vector(i,k-del_cnt) = vector(i,k); %to shift the RNTI values in the vector  after deleting decoded users
-                    ue_set(vector(i,j)) = 1;
-                else
-                    del_cnt = del_cnt+1;
-                    fprintf("deleted rnti %d from slot x %d\n",vector(i,k), i);
+    actUserNum = slotUeCnt(i)-1;
+    if (actUserNum == 1)
+
+        rntiForActiveUser = 0;
+        %If there is only 1 UE in the slot, sum of all rows in i. column will be equal to the RNTI of that user.
+        for j=1:1:50
+            rntiForActiveUser = rntiForActiveUser + slotVector(i,j);
+        end
+
+        %Apply SIC, search for decoded user and remove it from upcoming slot lists.
+        for k=i+1:1:numOfSlots
+            for l=1:1:50
+                if slotVector(k,l) == rntiForActiveUser
+                    slotVector(k,l) = 0;
+                    slotUeCnt(k) = slotUeCnt(k)-1;
                 end
             end
-            vector(i,1) = vector(i,1) - del_cnt; %to reduce the active user number in the slot
-            break;
         end
-        ue_set(vector(i,j)) = 1;
+        %search the decoded user in the previous slots as well and remove it.
+        if i>2
+            for t=i-1:-1:1
+                for s=1:1:50
+                    if slotVector(t,s) == rntiForActiveUser
+                        slotVector(t,s) = 0;
+                    end
+                end
+            end
+        end
+        for p=1:1:50
+            slotVector(i,p) = 0; %remove the user also from the current slot.
+        end
     end
 end
 
-%now the vector array that keeps number of active users and their RNTI
-%values are different array than userRntiwithScheduledSlotNumbers array.
-%How to correlate these two array?
+%now check the slotVector at the end and see if there is leftover user who cannot be decoded because of the interference.
+errorSum = 0;
+for z=1:1:numOfSlots
+    for y=1:1:50
+        errorSum = errorSum + slotVector(z,y);
+    end
+end
 
-%I believe SIC concept could happen if I remove already decoded users from
-%userRntiwithScheduledSlotNumbers array(from upcoming slots) after decoding
-%the users.
+if errorSum > 0
+    fprintf("Test failed");
+end
