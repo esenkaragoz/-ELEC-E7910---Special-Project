9752d5543f881de153d9c55a9c88138059e85fc6
esenkaragoz <99672463+esenkaragoz@users.noreply.github.com>
2022-04-22 22:04:09 +0300

    Update and rename Two_IC.m to ThreeFrame_2_IC.m

--- ThreeFrame_2_IC.m
+++ ThreeFrame_2_IC.m
@@ -1,15 +1,17 @@
 TwoICCode = nchoosek(1:1:26,5);
-numOfSlots = 26;    
-
+numOfFrame = 3;
+numOfSlots = 26*numOfFrame;
 loop_cnt = 1e6;
-max_simulated_users = 25;
+max_simulated_users = 60;
 results = zeros(1,max_simulated_users);
 tic
 parfor numOfActiveUsers = 1:1:max_simulated_users
 errorCounter = 0;
 for iteration = 1:loop_cnt
 slotVector = zeros(numOfSlots,numOfActiveUsers);
-S = TwoICCode(randsample(65780, numOfActiveUsers),:);
+Scopy = TwoICCode(randsample(65780, numOfActiveUsers),:);
+timeOffset = randi([0,26*(numOfFrame -1)],numOfActiveUsers,1);
+S = Scopy + timeOffset;
 for i=1:1:numOfActiveUsers
     slotVector(S(i,:),i) = ones(5,1);
 end
@@ -21,7 +23,7 @@ while 1
     [rows, cols] = find(quot);
     cols = unique(cols);
     a = size(cols);
-    slotVector(:,cols(:)) = zeros(26,a(1));
+    slotVector(:,cols(:)) = zeros(numOfSlots,a(1));
     if ~any(slotVector, 'all')
 	% Decoding is a success, break the while loop and go to next case
         break
