c718bb95a3093c985e23140fb3bdb1eb90d9ad8e
esenkaragoz <99672463+esenkaragoz@users.noreply.github.com>
2022-04-22 21:22:42 +0300

    Delete simulation_job.sh

--- simulation_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=24:00:00
-#SBATCH --mem=15G
-#SBATCH --cpus-per-task=24
-#SBATCH --output=simulation_output.out
-
-module load matlab
-
-srun matlab_multithread -nodisplay -r vectorized
